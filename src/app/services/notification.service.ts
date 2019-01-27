7325aedade93f8c8c7a440881e08c61ca3c0d829
Michael Karén <michael.karen@gmail.com>
2019-01-27 10:53:13 +0100

    comments

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -11,6 +11,7 @@ export class NotificationService {
     private zone: NgZone) { }
 
   showSuccess(message: string): void {
+    // Had an issue with the snackbar being ran outside of angular's zone.
     this.zone.run(() => {
       this.snackBar.open(message);
     });
@@ -18,6 +19,8 @@ export class NotificationService {
 
   showError(message: string): void {
     this.zone.run(() => {
+      // The second parameter is the text in the button. 
+      // In the third, we send in the css class for the snack bar.
       this.snackBar.open(message, 'X', {panelClass: ['error']});
     });
   }
